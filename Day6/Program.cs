518e21ee653e47269c49734e54e617e9e7191b8b
Adam Lear <aalear@users.noreply.github.com>
2022-12-06 00:37:10 -0500

    Day 6 the long(er) way
    It could still be longer than this if I were to do .Contains() checks and such rather than relying on .Distinct().

---
+++ Day6/Program.cs
@@ -0,0 +1,47 @@
+﻿var input = File.ReadAllLines("input.txt")[0];
+
+var markerIndex = 0;
+var marker = "";
+var markerFound = false;
+
+var messageIndex = 0;
+var message = "";
+var messageFound = false;
+
+foreach (var c in input)
+{
+    if (!markerFound) {
+        markerIndex++;
+        marker += c;
+        if (marker.Length == 4)
+        {
+            if (marker.Distinct().Count() == 4)
+            {
+                markerFound = true;
+                Console.WriteLine(markerIndex);
+            }
+            else
+            {
+                marker = marker[1..];
+            }
+        }
+    }
+
+    if (!messageFound)
+    {
+        messageIndex++;
+        message += c;
+        if (message.Length == 14)
+        {
+            if (message.Distinct().Count() == 14)
+            {
+                messageFound = true;
+                Console.WriteLine(messageIndex);
+            }
+            else
+            {
+                message = message[1..];
+            }
+        }
+    }
+}
